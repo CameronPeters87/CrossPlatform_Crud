678b0efe14f53fde0792ef3619d6d6fd2b7b3129
cameronpeters87 <cameronpeters87@gmail.com>
2020-09-12 11:10:19 +0200

    Added Xamarin Project

---
+++ CrossPlatform_Crud/Controllers/TestController.cs
@@ -0,0 +1,73 @@
+﻿using CrossPlatform_Crud.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace CrossPlatform_Crud.Controllers
+{
+    public class TestController : Controller
+    {
+        private string baseUrl = "https://localhost:44379/api/";
+        // GET: Test
+        public async Task<ActionResult> Index()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+
+                HttpResponseMessage responseMessage = await client.GetAsync("Customers");
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var response = responseMessage.Content
+                        .ReadAsStringAsync().Result;
+
+                    customers = JsonConvert.DeserializeObject<List<Customer>>(response);
+                }
+            }
+
+            return View(customers);
+        }
+
+        public async Task<ActionResult> PostCustomer()
+        {
+            Customer customer = new Customer
+            {
+                FirstName = "Jane",
+                Surname = "Doe",
+                Hire_Date = DateTime.Today,
+                Age = 40
+            };
+
+            var jsonCust = new JavaScriptSerializer().Serialize(customer);
+
+            HttpContent t = new StringContent(jsonCust, Encoding.UTF8, "application/json");
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+
+                HttpResponseMessage responseMessage = await client.PostAsync("Customers", t);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    return RedirectToAction("index");
+                }
+            }
+
+
+            return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+        }
+
+    }
+}
---
+++ CrossPlatform_Crud/Models/ArrayOfCustomers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CrossPlatform_Crud.Models
+{
+    public class ArrayOfCustomers
+    {
+        public ArrayOfCustomers()
+        {
+            Customers = new List<Customer>();
+        }
+        public List<Customer> Customers { get; set; }
+    }
+}
